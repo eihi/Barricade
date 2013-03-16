faff5d8562c5951a5e5ce705c30b1069b1bf73f2
Eihi <eihiara@gmail.com>
2013-03-16 21:08:07 +0100

    Barricade spel
    -model klassen

---
+++ Barricade/Model/Vak.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarricadeSpel
+{
+    class Vak
+    {
+        public Speler speler;   // data item
+        public Vak next;        // next vak
+        public Vak previous;    // previous vak
+    }
+}
